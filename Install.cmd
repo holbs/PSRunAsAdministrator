f74abf9db84b088d7bab5ecffc020bcf16cca82d
Aran <aranholbrook@outlook.com>
2022-04-30 18:41:40 +0100

    Added task path 'PSRunAsAdministrator'

--- Install.cmd
+++ Install.cmd
@@ -5,7 +5,7 @@ MD %SYSTEMROOT%\TreeSize\Logs
 MD %SYSTEMROOT%\TreeSize\Tools
 IF NOT EXIST %SYSTEMROOT%\TreeSize GOTO EXITERROR
 robocopy %~dp0 %SYSTEMROOT%\TreeSize\Tools CreateShortcut.ps1 MakeEvent.ps1 TreeSize.exe ServiceUIx64.exe ServiceUIx86.exe TreeSize.ico /z /np /LOG:%SYSTEMROOT%\TreeSize\Logs\Robocopy_TreeSize_Install.log /tee
-powershell Register-ScheduledTask -TaskName 'Create TreeSize shortcut' -Action (New-ScheduledTaskAction -Execute 'powershell' -Argument '-NoProfile -ExecutionPolicy bypass -File %SYSTEMROOT%\TreeSize\Tools\CreateShortcut.ps1') -Trigger (New-ScheduledTaskTrigger -AtLogon) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries)
+powershell Register-ScheduledTask -TaskName 'Create TreeSize shortcut' -TaskPath 'PSRunAsAdministrator\' -Action (New-ScheduledTaskAction -Execute 'powershell' -Argument '-NoProfile -ExecutionPolicy bypass -File %SYSTEMROOT%\TreeSize\Tools\CreateShortcut.ps1') -Trigger (New-ScheduledTaskTrigger -AtLogon) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries)
 powershell %~dp0CreateScheduledTasks.ps1
 schtasks /run /tn "Create TreeSize shortcut"
 SET DATETIME=%DATE%_%TIME%
